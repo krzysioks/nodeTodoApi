83c259eaa50f0341b2d1713b12c76d7d08679e6f
Krzysztof Pielak <krzysztofp@advaoptical.com>
2018-07-09 11:40:00 +0200

    setting app for heroku deployment

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -2,7 +2,8 @@ const mongoose = require('mongoose');
 
 //set global Promise
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017/todoApp');
+//mongodb://<dbuser>:<dbpassword>@ds131711.mlab.com:31711/nodetodoapp
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/todoApp');
 
 module.exports = {
     mongoose
